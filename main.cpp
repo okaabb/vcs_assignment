ce70b98d838a5cd267d595fd2b47af77e991371b
H <hanaaahmedhamed@Hanaas-MacBook-Pro.local>
2023-10-29 13:25:43 +0200

    average function

--- main.cpp
+++ main.cpp
@@ -10,9 +10,19 @@ int getSum(vector<int> v)
         return sum;
 }
 
+
+double getAverage(vector<int> v)
+{
+	if (v.empty()) return 0;
+        double sum = 0;
+        for (auto i:v) sum += i;
+        return sum / ( 1.0 * (int) v.size());
+}
+
 int main()
 {
         vector<int> myVec = {-5, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         cout << "Sum of my vector is: "<< getSum(myVec) << '\n';
+	cout << "Average Sum of my vector is: "<< getAverage(myVec) << '\n';
 	return 0;
 }
