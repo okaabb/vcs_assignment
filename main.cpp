a740b208da8dc1d7013c6a52c277e99b46ca190c
H <hanaaahmedhamed@Hanaas-MacBook-Pro.local>
2023-10-29 13:28:44 +0200

    minimum element function

--- main.cpp
+++ main.cpp
@@ -10,9 +10,18 @@ int getSum(vector<int> v)
         return sum;
 }
 
+int getMin(vector<int> v)
+{
+        int mn = 1e9;
+        for (auto i:v) mn = min(mn, i);
+        return mn;
+}
+
+
 int main()
 {
         vector<int> myVec = {-5, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         cout << "Sum of my vector is: "<< getSum(myVec) << '\n';
+	cout << "Minimum Element of my vector is: "<< getMin(myVec) << '\n';
 	return 0;
 }
